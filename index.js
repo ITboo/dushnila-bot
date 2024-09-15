8392d234541d9e78ad0fdd415d6eaf3c478e29cb
ITboo <annaacdc@tut.by>
2024-09-15 21:34:21 +0300

    finish bot, fix some errors

--- index.js
+++ index.js
@@ -1,79 +1,107 @@
-import "dotenv/config";
-import { Bot, Keyboard, InlineKeyboard, GrammyError, HttpError } from "grammy";
+import 'dotenv/config'
+import {
+  Bot,
+  Keyboard,
+  InlineKeyboard,
+  GrammyError,
+  HttpError,
+} from 'grammy';
+import { getRandomQuestion, getCorrectAnswer } from'./utils.js';
 
 const bot = new Bot(process.env.API_KEY);
 
-bot.command("start", async (ctx) => {
-  const startKeyBoard = new Keyboard()
-    .text("HTML & CSS")
-    .text("JavaScript")
-    .text("React")
+bot.command('start', async (ctx) => {
+  const startKeyboard = new Keyboard()
+    .text('HTML')
+    .text('CSS')
     .row()
-    .text("Случайный вопрос")
+    .text('JavaScript')
+    .text('React')
+    .row()
+    .text('Случайный вопрос')
     .resized();
   await ctx.reply(
-    "Привет! Готов помочь тебе подготовиться к собеседованию. Ты, наконец, вкатишься в IT и будешь получать лям в наносекунду \n Но это не точно"
+    'Привет! Я - Frontend Interview Prep Bot 🤖 \nЯ помогу тебе подготовиться к интервью по фронтенду',
   );
-  await ctx.reply("Выбирай тему:", {
-    reply_markup: startKeyBoard,
+  await ctx.reply('С чего начнем? Выбери тему вопроса в меню 👇', {
+    reply_markup: startKeyboard,
   });
 });
 
 bot.hears(
-  ["HTML & CSS", "JavaScript", "React", "Случайный вопрос"],
+  ['HTML', 'CSS', 'JavaScript', 'React', 'Случайный вопрос'],
   async (ctx) => {
+    const topic = ctx.message.text.toLowerCase();
+    const { question, questionTopic } = getRandomQuestion(topic);
+
     let inlineKeyboard;
-    inlineKeyboard = new InlineKeyboard()
-      .text("Получить ответ", JSON.stringify({
-        type: ctx.message.text,
-        questionId: 1
-      }))
-      .text("Отмена", "cancel");
-    await ctx.reply(`Что такое ${ctx.message.text}?`, { reply_markup: inlineKeyboard });
-  }
+
+    if (question.hasOptions) {
+      const buttonRows = question.options.map((option) => [
+        InlineKeyboard.text(
+          option.text,
+          JSON.stringify({
+            type: `${questionTopic}-option`,
+            isCorrect: option.isCorrect,
+            questionId: question.id,
+          }),
+        ),
+      ]);
+
+      inlineKeyboard = InlineKeyboard.from(buttonRows);
+    } else {
+      inlineKeyboard = new InlineKeyboard().text(
+        'Узнать ответ',
+        JSON.stringify({
+          type: questionTopic,
+          questionId: question.id,
+        }),
+      );
+    }
+
+    await ctx.reply(question.text, {
+      reply_markup: inlineKeyboard,
+    });
+  },
 );
 
-bot.on("callback_query:data", async (ctx) => {
-  if (ctx.callbackQuery.data === "cancel") {
-    await ctx.reply("Галя, у нас отмена");
+bot.on('callback_query:data', async (ctx) => {
+  const callbackData = JSON.parse(ctx.callbackQuery.data);
+
+  if (!callbackData.type.includes('option')) {
+    const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
+    await ctx.reply(answer, {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true,
+    });
     await ctx.answerCallbackQuery();
     return;
   }
-  const callBackData = JSON.parse(ctx.callbackQuery.data)
-  ctx.reply(`${callBackData.type} blahblahblah`)
+
+  if (callbackData.isCorrect) {
+    await ctx.reply('Верно ✅');
+    await ctx.answerCallbackQuery();
+    return;
+  }
+
+  const answer = getCorrectAnswer(
+    callbackData.type.split('-')[0],
+    callbackData.questionId,
+  );
+  await ctx.reply(`Неверно ❌ Правильный ответ: ${answer}`);
   await ctx.answerCallbackQuery();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 bot.catch((err) => {
   const ctx = err.ctx;
   console.error(`Error while handling update ${ctx.update.update_id}:`);
   const e = err.error;
   if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
+    console.error('Error in request:', e.description);
   } else if (e instanceof HttpError) {
-    console.error("Could not contact Telegram:", e);
+    console.error('Could not contact Telegram:', e);
   } else {
-    console.error("Unknown error:", e);
+    console.error('Unknown error:', e);
   }
 });
 
